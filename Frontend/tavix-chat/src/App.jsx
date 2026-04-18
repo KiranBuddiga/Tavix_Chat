2993f1f0ab2db12268d0d1a007cdc25cd6773fa6
Kirabuddiga <Kiranbuddiga5@gmail.com>
2026-04-18 07:08:52 +0530

    Frontend setup

---
+++ Frontend/tavix-chat/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-5xl font-medium">Hello Welcome to React Tavix APP</p>
+    </div>
+  );
+};
+
+export default App;
